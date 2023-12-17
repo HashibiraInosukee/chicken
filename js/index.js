dbf88cdf39f04f3f5e3ded7a376f7dcb0b662bc7
hashibira <113247885+HashibiraInosukee@users.noreply.github.com>
2023-12-18 00:43:07 +0700

    up

--- js/index.js
+++ js/index.js
@@ -12,12 +12,16 @@ $(".owl-carousel").owlCarousel({
       items: 1,
     },
 
-    600: {
-      items: 3,
+    767: {
+      items: 2,
     },
 
     1000: {
-      items: 5,
+      items: 3,
+    },
+
+    1024: {
+      items: 4,
     },
   },
 });
